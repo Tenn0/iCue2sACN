4bc1ef9cfa5c5a80309e9410ece98ba77e35cc3d
Matthew Bowley <m.bowley98@gmail.com>
2021-06-22 16:26:38 +0100

    Update main.py

--- src/main.py
+++ src/main.py
@@ -4,6 +4,7 @@ import json
 import sacn
 import time
 
+JSON_PATH = 'config.json'
 
 def setup_receiver(universe, device_index):
     name = sdk.get_device_info(device_index)
@@ -25,33 +26,37 @@ def setup_receiver(universe, device_index):
     print(f"Created sacn receiver for {name} on universe {universe}, with {len(led_ids)} leds")
 
 def get_free_universe():
-    print(conf.values())
-    for i in conf.values():
-        if i == None:
-            print(i)
-            return 1
-        else: 
-            return len(conf.values()) + 1
-  
+    return next(
+        i
+        for i in range(128)
+        if i not in conf.values()
+    )
+
+def load_config(config_path):
+    with open(config_path) as f:  #Config
+        return json.load(f)
+
+def save_config(config_path):
+    with open(config_path) as f:  #Config
+        json.dump(f)
 
 receiver = sacn.sACNreceiver() #sACN receiver  
 receiver.start()
 sdk = CueSdk() #Corsair iCue SDK
 sdk.connect()
 sdk.set_layer_priority(128)
-with open('config.json') as f:  #Config
-      conf = json.load(f)
-      print(conf)
+
+conf = load_config(JSON_PATH)
 
 device_count = sdk.get_device_count() #setup Corsair devices config
 for device_index in range(device_count):
     device_name = sdk.get_device_info(device_index)
-    exists = device_name.model in conf
-    if exists == False:
+    if device_name.model in conf:
         universe = get_free_universe()
-        conf.update({device_name.model: universe})   
+        conf.update({device_name.model: universe}) 
         print(f"conf= {conf}")
     else:
         universe = conf[device_name.model] 
+    save_config(JSON_PATH)
     setup_receiver(universe, device_index)
 
