a88dc6cda33d95bb303d3aaad94a2a2b264c6fd2
Matthew Bowley <m.bowley98@gmail.com>
2021-06-22 16:29:00 +0100

    Update main.py

--- src/main.py
+++ src/main.py
@@ -38,7 +38,7 @@ def load_config(config_path):
 
 def save_config(config_path):
     with open(config_path) as f:  #Config
-        json.dump(f)
+        json.dump(conf, f)
 
 receiver = sacn.sACNreceiver() #sACN receiver  
 receiver.start()
