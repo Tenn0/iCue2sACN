2e81546e3b24d2070e61c0f6a7798aff58b0d65d
tenn0 <rougerom9@gmail.com>
2021-06-29 18:10:37 +0200

    add mqtt connection and dynamically subscribe to device topics

--- src/main.py
+++ src/main.py
@@ -1,17 +1,18 @@
-from os import times
 from cuesdk import CueSdk
 import json
 import sacn
 import time
 import paho.mqtt.client as mqtt
-
+import os
 
 DEVICE_PATH = 'config.json'
 MQTT_PATH = 'mqtt.json'
 
 def on_connect(client, userdata, flags, rc):
       print("Connected with result code "+str(rc))
-      client.subscribe("hi/#")
+      client.subscribe(mqtt_base_topic) #subscribe to base topic
+      client.publish(mqtt_base_topic, str(load_config(DEVICE_PATH))) #dump device_config into base topic
+
 
 def on_message(client, userdata, msg):
     payload = msg.payload.decode("utf-8")
@@ -48,32 +49,100 @@ def get_free_universe():
     )
 
 def load_config(config_path):
-    with open(config_path) as f:  #Config
+    if not os.path.isfile(config_path):  #Create the file if not present
+        open(config_path, "w+")
+    with open(config_path) as f:  #load the config file
         try:
             return json.load(f)
-        except OSError:
-            print("file not there, creating it")
-            open(config_path, "x")
         except json.JSONDecodeError:
-            return {}
-            
-
+            return {}   
+    
 def save_config(config_path):
     with open(config_path, "w", encoding="utf-8") as f:  # Save config
         json.dump(
-            conf,2
+            conf,
             f, 
             ensure_ascii=False,
             sort_keys=True,
             indent=4
         )
 
-conf = load_config(DEVICE_PATH)  #load device confiug
+def subscribe_device_command_topics(device, base_topic):  #subscribe to device state topic and publish on message dynically
+    command_topic = str(str(base_topic) + "/" + str(device) + "/command")
+    state_topic = str(str(base_topic) + "/" + str(device) + "/state")
+    print(f"subscribed to {command_topic}")
+    print(f"puiblishing to {state_topic}")
+    client.subscribe(command_topic)
+    def callback(client, userdata, msg):
+        payload = msg.payload.decode("utf-8")
+        if payload == "on":
+           print("on")
+           client.publish(state_topic, payload=payload, qos=0, retain=True) 
+        else:
+            print("off")
+            client.publish(state_topic, payload=payload, qos=0, retain=True)
+    
+    client.message_callback_add(command_topic, callback)
+
+def subscribe_device_effect_command_topics(device, base_topic):  #subscribe to device state topic and publish on message dynically
+    command_topic = str(str(base_topic) + "/" + str(device) + "/effect/command")
+    state_topic = str(str(base_topic) + "/" + str(device) + "//effect/state")
+    print(f"subscribed to {command_topic}")
+    print(f"puiblishing to {state_topic}")
+    client.subscribe(command_topic)
+    def callback(client, userdata, msg):
+        payload = msg.payload.decode("utf-8")
+        if payload == "on":
+           print("on")
+           client.publish(state_topic, payload=payload, qos=0, retain=True) 
+        else:
+            print("off")
+            client.publish(state_topic, payload=payload, qos=0, retain=True)
+    
+    client.message_callback_add(command_topic, callback)
+
+def subscribe_device_color_command_topics(device, base_topic):  #subscribe to device state topic and publish on message dynically
+    command_topic = str(str(base_topic) + "/" + str(device) + "/color/command")
+    state_topic = str(str(base_topic) + "/" + str(device) + "/color/state")
+    print(f"subscribed to {command_topic}")
+    print(f"puiblishing to {state_topic}")
+    client.subscribe(command_topic)
+    def callback(client, userdata, msg):
+        payload = msg.payload.decode("utf-8")
+        if payload == "on":
+           print("on")
+           client.publish(state_topic, payload=payload, qos=0, retain=True) 
+        else:
+            print("off")
+            client.publish(state_topic, payload=payload, qos=0, retain=True)
+    
+    client.message_callback_add(command_topic, callback)
+
+def subscribe_device_brightness_command_topics(device, base_topic):  #subscribe to device state topic and publish on message dynically
+    command_topic = str(str(base_topic) + "/" + str(device) + "/brightness/command")
+    state_topic = str(str(base_topic) + "/" + str(device) + "/brightness/state")
+    print(f"subscribed to {command_topic}")
+    print(f"puiblishing to {state_topic}")
+    client.subscribe(command_topic)
+    def callback(client, userdata, msg):  #handle msg
+        payload = msg.payload.decode("utf-8")
+        if payload == "on":
+           print("on")
+           client.publish(state_topic, payload=payload, qos=0, retain=True) 
+        else:
+            print("off")
+            client.publish(state_topic, payload=payload, qos=0, retain=True)
+    
+    client.message_callback_add(command_topic, callback)
+
+
+conf = load_config(DEVICE_PATH)  #load device config
 mqtt_conf = load_config(MQTT_PATH) #load mqtt config
 mqtt_broker_ip = mqtt_conf['ip']  #setup mqtt
 mqtt_broker_port = mqtt_conf['port']
 mqtt_user = mqtt_conf['username']
 mqtt_pass = mqtt_conf['password']
+mqtt_base_topic = mqtt_conf['base_topic']
 client = mqtt.Client()
 client.on_connect = on_connect
 client.on_message = on_message
@@ -100,4 +169,10 @@ for device_index in range(device_count):
         universe = conf[device_name.model] 
     save_config(DEVICE_PATH)
     setup_receiver(universe, device_index)
+    subscribe_device_command_topics(device_name.model, mqtt_base_topic)
+    subscribe_device_brightness_command_topics(device_name.model, mqtt_base_topic)
+    subscribe_device_color_command_topics(device_name.model, mqtt_base_topic)
+    subscribe_device_effect_command_topics(device_name.model, mqtt_base_topic)
 
+
+client.loop_forever()
