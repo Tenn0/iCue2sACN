324e5f6e7fa8113ad169c74d9fb4208d97a572e3
tenn0 <rougerom9@gmail.com>
2021-07-01 02:17:56 +0200

    simplify mqtt device handling, adding home assistant auto discovery, handling layer priority and exclusive control via mqtt

--- src/main.py
+++ src/main.py
@@ -123,91 +123,98 @@ def subscribe_device_effect_command_topics(device, base_topic, universe):  #hand
             
     client.message_callback_add(command_topic, callback)
 
-def subscribe_device_color_command_topics(device, base_topic):  #subscribe to device state topic and publish on message dynically
-    command_topic = str(str(base_topic) + "/" + str(device) + "/color/command")
-    state_topic = str(str(base_topic) + "/" + str(device) + "/color/state")
-    print(f"subscribed to {command_topic}")
-    print(f"puiblishing to {state_topic}")
-    client.subscribe(command_topic)
-    def callback(client, userdata, msg):
-        payload = msg.payload.decode("utf-8")
-        if payload == "on":
-           print("on")
-           client.publish(state_topic, payload=payload, qos=0, retain=True) 
-        else:
-            print("off")
-            client.publish(state_topic, payload=payload, qos=0, retain=True)
-    
-    client.message_callback_add(command_topic, callback)
 
-def subscribe_device_brightness_command_topics(device, base_topic):  #subscribe to device state topic and publish on message dynically
-    command_topic = str(str(base_topic) + "/" + str(device) + "/brightness/command")
-    state_topic = str(str(base_topic) + "/" + str(device) + "/brightness/state")
-    print(f"subscribed to {command_topic}")
-    print(f"puiblishing to {state_topic}")
-    client.subscribe(command_topic)
-    def callback(client, userdata, msg):  #handle msg
-        payload = msg.payload.decode("utf-8")
-        if payload == "on":
-           print("on")
-           client.publish(state_topic, payload=payload, qos=0, retain=True) 
-        else:
-            print("off")
-            client.publish(state_topic, payload=payload, qos=0, retain=True)
-    
-    client.message_callback_add(command_topic, callback)
 
-def publish_device_info(mqtt_base_topic, device_name, device_type, led_count, universe):
+def publish_device_info(mqtt_base_topic, device_name, topic_name):
+    device_info_topic = str(str(mqtt_base_topic)+"/"+str(topic_name)+"/config")
     data = {}
     data['name'] = str(device_name)
-    data['type'] = str(device_type)
-    data['led_count'] = str(led_count)
-    data['universe'] = str(universe)
+    data['unique_id'] = str(device_name + "_iCUE2sACN")
+    data['state_topic'] = str(str(mqtt_base_topic) + "/" + str(topic_name)+ "/state")
+    data['command_topic'] = str(str(mqtt_base_topic) + "/" +str(topic_name)+ "/command")
+    data['schema'] = "json"
+    data['brightness'] = "true"
+    data['color_mode'] = "true"
+    data['supported_color_modes'] = ["rgb"]
+    data['effect'] = "true"
+    data['effect_list'] = ["None", "sACN", "iCUE"]
+    data['device'] = device_data
     payload = json.dumps(data)
-    device_info_topic = str(str(mqtt_base_topic)+"/"+str(device_name)+"/config")
     print(f"publishing device info for: {device_name}")
     print(f"at topic: {device_info_topic}")
     client.publish(device_info_topic, payload, qos=0, retain=True)
 
-def subscribe_use_exclusive_control(base_topic):
-    topic = str(str(base_topic) + "/switch/exclusive_control")
+def setup_use_exclusive_control(base_topic):
+    topic = str(str(base_topic) + "iCUE2sACN/iCUE_exclusive_control")
     command_topic = str(str(topic) + str("/command"))
     state_topic = str(topic + "/state") 
+    config_topic = topic + "/config"
+    data = {}
+    data['name'] = "iCUE Exclusive Control"
+    data['unique_id'] = str(data['name'] + "_iCUE2sACN")
+    data['state_topic'] = state_topic
+    data['command_topic'] = command_topic
+    data['device'] = device_data
+    config_payload = json.dumps(data)
+    client.publish(config_topic, config_payload, qos=0, retain=True)
     client.subscribe(command_topic)
     def callback(client, userdata, msg):
         payload = msg.payload.decode("utf-8")
-        print(payload)
-        if payload == "True":
+        if payload == "ON":
             sdk.request_control()
-            client.publish(state_topic, payload, qos=0, retain=True)
-            print("requesing exclusive control")
-        if payload == "False":
+        if payload =="OFF":
             sdk.release_control()
-            client.publish(state_topic, payload, qos=0, retain=True)
-            print("releasing exclusive control")
+        client.publish(state_topic, payload, qos=0, retain=True)
+        
     client.message_callback_add(command_topic, callback)
 
-def subscribe_layer_priority(base_topic):
+def setup_layer_priority(base_topic):
     topic = str(str(base_topic) + "/light/iCUE_device_layer")
     command_topic = str(str(topic) + str("/command"))
-    print(command_topic)
+    config_topic = topic + "/config"
     state_topic = str(topic + "/state") 
     client.subscribe(command_topic)
+    data = {}
+    data['name'] = "iCUE Layer Priority"
+    data['unique_id'] = str(data['name'] + "_iCUE2sACN")
+    data['state_topic'] = state_topic
+    data['command_topic'] = command_topic
+    data['brightness'] = "true"
+    data['schema'] = "json"
+    data['device'] = device_data
+    config_payload = json.dumps(data)
+    client.publish(config_topic, config_payload, qos=0, retain=True )
     def callback(client, userdata, msg):
         payload = msg.payload.decode("utf-8")
-        payload = int(payload)
-        if payload > 255:
-            payload = 255
-        if payload < 0:
-            payload = 0
-        client.publish(state_topic, payload, qos=0, retain=True)
-        sdk.set_layer_priority(payload)
+        payload2 = json.loads(payload)
+        print(str(payload2))
+        state_payload = {}
+        state_payload['state'] = payload2['state']
+        if "brightness" in payload2 :
+            state_payload['brightness'] = payload2['brightness']
+        #   last_layer_priority = state_payload["brightness"]
+            sdk.set_layer_priority(payload2['brightness'])
+        #else: 
+        #   last_layer_priority = 0
+        if payload2['state'] == "OFF":
+            sdk.set_layer_priority(0)
+        if payload2['state'] == "ON":
+        #  state_payload['brightness'] = last_layer_priority
+        # sdk.set_layer_priority(last_layer_priority)
+            state_payload["brightness"] = "128"
+            sdk.set_layer_priority(128)
+        state_payload = json.dumps(state_payload)
+        client.publish(state_topic, state_payload, qos=0, retain=True)
+        
     
     client.message_callback_add(command_topic, callback)
 
 
 
-
+device_data = {}
+device_data['name'] = "iCUE2sACN"
+device_data['identifiers'] = "iCUE2sACN"
+device_data['manufacturer'] = "tenn0"
 conf = load_config(DEVICE_PATH)  #load device config
 mqtt_conf = load_config(MQTT_PATH) #load mqtt config
 mqtt_broker_ip = mqtt_conf['ip']  #setup mqtt
@@ -215,7 +222,7 @@ mqtt_broker_port = mqtt_conf['port']
 mqtt_user = mqtt_conf['username']
 mqtt_pass = mqtt_conf['password']
 mqtt_base_topic = mqtt_conf['base_topic']
-light_topic = mqtt_base_topic + "/light"
+light_topic = mqtt_base_topic + "/light/iCUE2sACN"
 client = mqtt.Client()
 client.on_connect = on_connect
 client.on_message = on_message
@@ -229,8 +236,8 @@ sdk = CueSdk() #Corsair iCue SDK
 sdk.connect()
 sdk.set_layer_priority(128)
 
-subscribe_use_exclusive_control(mqtt_base_topic)
-subscribe_layer_priority(mqtt_base_topic)
+setup_use_exclusive_control(light_topic)
+setup_layer_priority(light_topic)
 
 device_count = sdk.get_device_count() #setup Corsair devices config
 for device_index in range(device_count):
@@ -244,11 +251,9 @@ for device_index in range(device_count):
         universe = conf[device_name.model] 
     save_config(DEVICE_PATH)
     setup_receiver(universe, device_index)
-
-    subscribe_device_command_topics(device_name.model, light_topic)
-    subscribe_device_brightness_command_topics(device_name.model, light_topic)
-    subscribe_device_color_command_topics(device_name.model, light_topic)
-    subscribe_device_effect_command_topics(device_name.model, light_topic, universe)
-    publish_device_info(light_topic, device_name.model, device_name.type, device_name.led_count, universe )
+    device_topic_name_short = device_name.model.replace(" ", "_")
+    subscribe_device_command_topics(device_topic_name_short, light_topic)
+    subscribe_device_effect_command_topics(device_topic_name_short, light_topic, universe)
+    publish_device_info(light_topic, device_name.model, device_topic_name_short)
 
 client.loop_forever()
